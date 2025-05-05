bba9f98a40233e38094bdcb72c6c842018d6a404
ljvdhooft <ljvdhooft@gmail.com>
2025-05-05 19:17:52 +0200

    updated setup.py

--- library/setup.py
+++ library/setup.py
@@ -1,23 +1,23 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='ableton_set_builder',
-    version='0.1.0',
+    name="ableton_set_builder",
+    version="0.1.0",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'xmltodict',
+        "xmltodict",
     ],
-    author='Lars & Manuel',
-    author_email='me@manuelmol.nl',
-    description='A package to build Ableton Live sets programmatically.',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/ableton_set_builder',
+    author="Lars & Manuel",
+    author_email="me@manuelmol.nl",
+    description="A package to build Ableton Live sets programmatically.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/PCO-Ableton/ableton_set_builder",
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
 )
